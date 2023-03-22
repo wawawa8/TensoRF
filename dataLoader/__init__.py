2360b965cfd56ff83b4b7ec811279cb32c3aa6de
zongtai <zol010@ucsd.edu>
2023-03-22 15:47:29 -0700

    fix bugs

--- dataLoader/__init__.py
+++ dataLoader/__init__.py
@@ -3,11 +3,12 @@ from .blender import BlenderDataset
 from .nsvf import NSVF
 from .tankstemple import TanksTempleDataset
 from .your_own_data import YourOwnDataset
-
+from realdata import RealdataDataset
 
 
 dataset_dict = {'blender': BlenderDataset,
                'llff':LLFFDataset,
                'tankstemple':TanksTempleDataset,
                'nsvf':NSVF,
+               'realdata':RealdataDataset,
                 'own_data':YourOwnDataset}
